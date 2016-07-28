f6fba6b69117fd295d9f7b60209d0f0f6d1b8070
alegume <alexandreabreu@comp.ufla.br>
2016-07-27 21:22:57 -0300

    Criacao do repositorio

---
+++ insert_sort.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+#define N 10
+
+void imprimir(int vet[]) {
+	for (int i = 0; i < N; i++)
+		printf("%d - ", vet[i]);
+	printf("\n");
+}
+
+void inicializar(int *vet) {
+	for (int i = N - 1; i >= 0; i--)
+		vet[i] = N - i;
+}
+
+int main() {
+	int vet[N], aux, j;
+	inicializar(&vet[0]);
+	imprimir(vet);
+
+	for (int i = 1; i < N; i++) { // A partir do segundo elemento
+			aux = vet[i]; // Copia do elemento a ser movido
+		j = i - 1; // Posicao anterior ao elemento a ser movido
+		// Ate encontrar em elemento menor que aux ou ate a 1a posicao
+		while (j >= 0 && vet[j] > aux) { 
+			vet[j+1] = vet[j]; // Move elementos para a direita
+			j--;
+ 		}
+		vet[j+1] = aux; // Copia elemento para sua posicao
+	}
+
+	imprimir(vet);
+
+	return 0;
+}
---
+++ select_sort.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+#define N 15
+
+void imprimir(int vet[]) {
+	for (int i = 0; i < N; i++)
+		printf("%d - ", vet[i]);
+	printf("\n");
+}
+
+void inicializar(int *vet) {
+	for (int i = N - 1; i >= 0; i--)
+		vet[i] = N - i;
+}
+
+int main() {
+	int vet[N], min, aux;
+	inicializar(&vet[0]);
+	imprimir(vet);
+
+	for (int i = 0; i < N - 1; i++) { // O ultimo elemento ja estara em sua posicao
+		min = i; // posicao para trocar o menor elemento da SUBlista 
+		for (int j = i + 1; j < N; j++) { // Verifica o menor elemento na sublista [j..N]
+			if (vet[j] < vet[min])
+				min = j; // Atualiza posicao do menor
+		}
+		aux = vet[min]; // Auxiliar armazena menor valor da sublista
+		vet[min] = vet[i]; // Elemento na posicao do menor vai para a posicao do menor
+		vet[i] = aux; // Menor eh transferido para sua posicao
+	}
+
+	imprimir(vet);
+
+	return 0;
+}
